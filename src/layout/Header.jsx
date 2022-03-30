0f30e2f71e227d261943c454fbc734a106398a08
Vatasi <tifu7311@yandex.ru>
2022-03-30 15:25:26 +0400

    movie ref

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header () {
 return  <nav className="light-blue darken-3">
 <div className="nav-wrapper">
-  <a href="/" className="brand-logo">React Movies</a>
+  <a href="https://vatasi73.github.io/ReactMovies/" className="brand-logo">React Movies</a>
   <ul id="nav-mobile" className="right hide-on-med-and-down">
     <li><a href="!#">Repo</a></li>
   </ul>
